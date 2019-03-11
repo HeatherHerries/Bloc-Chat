4bb6f409f26f56f51bb76fce045b93d88dd6b9da
Heather Herries <hherries@yahoo.com>
2019-03-10 21:43:04 -0600

    Checkpoint: List Messages: completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
 
 
@@ -16,13 +17,40 @@ import RoomList from './components/RoomList';
   firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRoom: '',
+      activeRoomId: '',
+      user: ''
+    }
+  }
+
+  updateRoom(roomId) {
+    if (roomId === '') {
+      this.setState({activeRoomId: '', activeRoom: ''})
+    } else {
+      this.setState({activeRoomId: roomId.key, activeRoom: roomId.name});
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Bloc Chat</h1>
         </header>
-        <RoomList firebase={firebase} />
+        <RoomList firebase={firebase}
+        updateRoom={this.updateRoom.bind(this)}
+        activeRoomId={this.state.activeRoomId}
+        activeRoom={this.state.activeRoom} />
+
+        <section className="message-list">
+          <MessageList firebase={firebase}
+            activeRoom={this.state.activeRoom}
+            activeRoomId={this.state.activeRoomId} />
+        </section>
 
       </div>
     );
